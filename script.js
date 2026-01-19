c3d6ab05d4d934ad65b668e5313f1c1ef99584c4
Mai Trinh Vu <131637595+ovixm@users.noreply.github.com>
2026-01-19 16:51:03 +0000

    changed window width + added basic taskbar and apps

---
+++ script.js
@@ -0,0 +1,4 @@
+redButton = document.getElementById('red-btn');
+yellowButton = document.getElementById('yellow-btn');
+greenButton = document.getElementById('green-btn');
+windowDiv = document.querySelector('.window');
