f579a461b3c4b53bfdf7d9e29af6259661970072
Mai Trinh Vu <131637595+ovixm@users.noreply.github.com>
2026-01-19 20:31:58 +0000

    add sound, music icon, and time/date

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
-redButton = document.getElementById('red-btn');
-yellowButton = document.getElementById('yellow-btn');
-greenButton = document.getElementById('green-btn');
+//time and date
+let timeAndDate = document.getElementById('timedate');
+
+function timedate() {
+  const now = new Date();
+  const DateTimeFormat = new Intl.DateTimeFormat(undefined, {dateStyle: 'short', timeStyle: 'long'});
+
+  timeAndDate.textContent = DateTimeFormat.format(now);
+
+  requestAnimationFrame(timedate);
+}
+
+timedate();
+
+//windows and apps
+let error = new Audio('sounds/erro.mp3');
+
+redButton = document.querySelector('.btn.red');
+yellowButton = document.querySelector('.btn.yellow');
+greenButton = document.querySelector('.btn.green');
 windowDiv = document.querySelector('.window');
+
+internetApp = document.getElementById('internetApp');
+
+redButton.addEventListener('click', () => {
+  windowDiv.style.visibility = 'hidden';
+});
+
+internetApp.addEventListener('click', () =>{
+  if (windowDiv.style.visibility == 'visible')
+  {
+    error.play();
+  }
+  else
+  {
+    windowDiv.style.visibility = 'visible';
+  }
+});
+
